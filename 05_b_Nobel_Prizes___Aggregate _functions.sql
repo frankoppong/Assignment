72273c44d8a621314c8f2aabc2750c388da517b2
frankoppong <60891734+frankoppong@users.noreply.github.com>
2023-05-17 10:14:06 +0000

    SQL Zoo
    SQL solution to challenges from http://sqlzoo.net.

---
+++ 05_b_Nobel_Prizes___Aggregate _functions.sql
@@ -0,0 +1,82 @@
+--1. Show the total number of prizes awarded
+
+SELECT COUNT(winner)
+FROM nobel;
+
+--2. List each subject - just once
+
+SELECT DISTINCT(subject)
+FROM nobel;
+
+--3. Show the total number of prizes awarded for Physics.
+
+SELECT COUNT(winner) AS Award4Physics
+FROM nobel
+WHERE subject = 'Physics';
+
+
+--4. For each subject show the subject and the number of prizes.
+
+SELECT subject, COUNT(winner) AS NumOfPrizes
+FROM nobel
+GROUP BY subject;
+
+--5. For each subject show the first year that the prize was awarded.
+
+SELECT subject, MIN(yr) AS FirstYear
+FROM nobel
+GROUP BY subject;
+
+--6. For each subject show the number of prizes awarded in the year 2000
+
+SELECT subject, COUNT(winner) AS NumOfPrizes
+FROM nobel
+WHERE yr = 2000
+GROUP BY subject;
+
+-- 7. Show the number of different winners for each subject.
+
+SELECT subject, COUNT(DISTINCT winner) AS DifferentWinners
+FROM nobel 
+GROUP BY subject;
+
+--8. For each subject show how many years have had prizes awarded.
+
+SELECT subject, COUNT(DISTINCT yr) AS AwarededPrize
+FROM nobel
+GROUP BY subject;
+
+--9. Show the years in which three prizes were given for Physics.
+
+SELECT yr
+FROM nobel
+WHERE subject = 'Physics'
+GROUP BY yr
+HAVING COUNT(winner) = 3;
+
+--10. Show winners who have won more than once
+
+SELECT winner
+FROM nobel
+GROUP BY winner
+HAVING COUNT(winner) > 1;
+
+
+--11. Show winners who have won more than one subject.
+
+SELECT winner
+FROM nobel 
+GROUP BY winner 
+HAVING COUNT(DISTINCT subject) > 1
+
+--12. Show the year and subject where 3 prizes were given. Show only years 2000 onwards.
+
+--11. Show the year and subject where 3 prizes were given. Show only years 2000 onwards.
+
+SELECT yr, subject
+FROM nobel
+WHERE yr >=2000
+GROUP BY yr, subject
+HAVING COUNT(winner) = 3;
+
+
